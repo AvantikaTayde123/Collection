3a6a1be2c4025419b5ac69649323cc236d0a14e7
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2024-10-05 21:33:53 +0530

    Create Example9.java

---
+++ Example9.java
@@ -0,0 +1,18 @@
+//Create a Stack of 5 numbers. Pop all the elements one by one and print each element as it is removed.
+import java.util.Stack;
+public class ArrayEx9 {
+    public static void main(String[] args) {
+    Stack<Integer> st = new Stack<>();
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
+while(!st.isEmpty()){
+    int ele =st.pop();
+    System.out.println(ele);
+}
+
+
+    }   
+}
